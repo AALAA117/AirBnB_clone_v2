ac7335e5e56db091922be2d6ff7e7c261733fce9
alaa <alaamglal@gmail.com>
2024-05-25 18:50:02 -0700

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,15 +22,18 @@ def do_deploy(archive_path):
 
         # Uncompress the archive to the folder
         filename = path.basename(archive_path)
-        name, ext = path.splitext(filename)
+        name, ext = os.path.splitext(filename)
         if ext != '.tgz':
-            return(False)
+            return (False)
         release_path = "/data/web_static/releases/{}".format(name)
         run("sudo mkdir -p {}".format(release_path))
         run("sudo tar -xzf /tmp/{} -C {}".format(filename, release_path))
 
         # Delete the archive from the web server
-        run("sudo rm -r /tmp/{}".format(filename))
+        run("sudo rm /tmp/{}".format(filename))
+        # Move the contents of the web_static directory
+        # one level up to get desired structure
+        run("sudo mv {}/web_static/* {}".format(release_path, release_path))
 
         # Delete the symbolic link /data/web_static/current
         run("sudo rm -rf /data/web_static/current")
