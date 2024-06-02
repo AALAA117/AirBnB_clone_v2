94f778bcdccae728f6b2bdd70db1f0d038a6f767
alaa <alaamglal@gmail.com>
2024-06-02 14:27:40 -0700

    done

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """display “Hello HBNB!”"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display “HBNB”"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_c_text(text):
+    return("C {}".format(text.replace("_", " ")))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def show_python_text(text="is cool"):
+    return("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return (render_template("5-number.html", number=n))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host='0.0.0.0')
